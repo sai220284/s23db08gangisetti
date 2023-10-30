32034eee49eebf1886a6cc7199a9cbdba87a4436
Sai Teja Gangisetti <143573794+sai220284@users.noreply.github.com>
2023-10-30 18:34:46 -0500

    working shoes pug

--- routes/shoes.js
+++ routes/shoes.js
@@ -1,25 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// Define your Shoes class (you can use a database or an array)
-var shoesData = [
-  { brand: 'Nike', size: 10, price: 100 },
-  { brand: 'Adidas', size: 9, price: 85 },
-  { brand: 'Puma', size: 8, price: 75 },
-];
-
-// Define a route to render the "Shoes" page
-app.get('/shoes', (req, res) => {
-  res.render('shoes', {
-    title: 'Search Results - Shoes',
-    shoes: shoesData, // Pass the shoe data to the Pug template
-  });
-});
-
-
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shoes', { title: 'Express' });
+  res.render('shoes', { title: 'Search Results shoes' });
 });
 
 module.exports = router;
-
