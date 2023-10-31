6563f935f867035019785927b6c754436c0980ea
Sai Teja Gangisetti <143573794+sai220284@users.noreply.github.com>
2023-10-30 19:13:43 -0500

    bonus and choose

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var shoesRouter = require('./routes/shoes');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
+var snakeRouter = require('./routes/snake');
 
 
 var app = express();
@@ -26,6 +28,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/shoes', shoesRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
+app.use('/snake', snakeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
